a23fd87f0a0eca0cce1a974b97880f4d02833b2b
druyoung <40210727+druyoung@users.noreply.github.com>
2018-06-19 16:32:58 -0600

    Update dockerscript.sh

--- dockerscript.sh
+++ dockerscript.sh
@@ -2,6 +2,5 @@
 # git clone https://github.com/druyoung/WTW.git drewgit
 # install latest docker
 wget -qO- https://get.docker.com/ | sh
-cd drewgit
 # start nginx container with mount points
 docker container run -d -p 3200:3200 -v $(pwd):/etc/nginx/config.d/ -v $(pwd): /var/lib/www/html/errors --name druginx nginx
