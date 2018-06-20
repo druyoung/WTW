e0f221c69d3f5c909328c194175e30956138562b
druyoung <40210727+druyoung@users.noreply.github.com>
2018-06-19 20:58:16 -0600

    Delete dockerscript.sh

--- dockerscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# git clone https://github.com/druyoung/WTW.git drewgit
-# install latest docker
-wget -qO- https://get.docker.com/ | sh
-# start nginx container with mount points
-docker container run -d -p 3200:3200 -v $(pwd):/etc/nginx/conf.d -v $(pwd):/usr/share/nginx/html --name druginx nginx
